bd818d4392f539b134d6614b4dc6423361843c22
Isharani01 <ishakhizar2709@gmail.com>
2026-09-20 23:50:47 +0500

    task14 click

--- LAB TASK11/script.js
+++ LAB TASK11/script.js
@@ -1,3 +1,6 @@
-let label="Read";
+let label = "Read";
 console.log(label);
-document.querySelector("#status");
+
+document.querySelector("#mark").addEventListener("click", function () {
+  document.querySelector("#status").textContent = label;
+});
